e6123a17dfc955e4b418a1b54b76a8669a399bdb
Søren Bech <soren-bech@outlook.com>
2025-01-31 10:16:40 +0100

    Add detailed culture logging and new Debug config
    Updated `MAUI_DecimalPointTesting.sln` to include a new deployment configuration for the Debug build. Enhanced the `Button_Clicked` method in `MainPage.xaml.cs` to log detailed culture information, including platform-specific details for iOS and Android using conditional compilation directives.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -15,8 +15,45 @@ public partial class MainPage : ContentPage
 
     private void Button_Clicked(object sender, EventArgs e)
     {
-        var numberFormat = CultureInfo.CurrentCulture.NumberFormat;
-        Debug.WriteLine($"NumberDecimalSeparator: {numberFormat.NumberDecimalSeparator}");
-        Debug.WriteLine($"NumberGroupSeparator: {numberFormat.NumberGroupSeparator}");
+        Debug.WriteLine("Below output is generated using .NET");
+        
+        var currentCulture = CultureInfo.CurrentCulture;
+        var numberFormat = currentCulture.NumberFormat;
+
+        Debug.WriteLine($"Device Culture Name : {currentCulture.Name}");
+        Debug.WriteLine($"Device Display name : {currentCulture.DisplayName}");
+        Debug.WriteLine($"Device Language name : {currentCulture.TwoLetterISOLanguageName}");
+        
+        Debug.WriteLine(" ");
+        Debug.WriteLine($"Device NumberDecimalSeparator: {numberFormat.NumberDecimalSeparator}");
+        Debug.WriteLine($"Device NumberGroupSeparator: {numberFormat.NumberGroupSeparator}");
+
+#if IOS
+        Debug.WriteLine(" "); Debug.WriteLine(" ");
+        Debug.WriteLine("Below output is generated using iOS native code.");
+
+        var locale = Foundation.NSLocale.CurrentLocale;
+        
+        Debug.WriteLine($"Device CurrentLocale.LanguageCode: {locale.LanguageCode}");
+        Debug.WriteLine($"Device CurrentLocale.CountryCode: {locale.CountryCode}");
+        Debug.WriteLine(" ");
+        Debug.WriteLine($"Device CurrentLocale.DecimalSeparator: {locale.DecimalSeparator}");
+        Debug.WriteLine($"Device CurrentLocale.GroupingSeparator: {locale.GroupingSeparator}");
+#endif
+#if ANDROID
+        Debug.WriteLine(" "); Debug.WriteLine(" ");
+        Debug.WriteLine("Below output is generated using Android native code");
+        
+        var locale = Java.Util.Locale.Default;
+        Android.Icu.Text.DecimalFormatSymbols decimalFormat = new();
+
+        Debug.WriteLine($"Device Language: {locale.Language}");
+        Debug.WriteLine($"Device Country: {locale.Country}");
+        Debug.WriteLine($"Device Display Language: {locale.DisplayLanguage}");
+        Debug.WriteLine($"Device Display Country: {locale.DisplayCountry}");
+        Debug.WriteLine(" ");
+        Debug.WriteLine($"Device DecimalSeparator: {decimalFormat.DecimalSeparator}");
+        Debug.WriteLine($"Device GroupingSeparator: {decimalFormat.GroupingSeparator}"); 
+#endif
     }
 }
